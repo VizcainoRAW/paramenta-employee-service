1a01e9333c9ea72320e75ee17966ea205df3c775
ivan <ivizcain4@cuc.edu.co>
2025-08-14 12:09:33 -0500

    feat: bootstrap Paramenta employee-service with Spring Initializr

---
+++ src/test/java/com/paramenta/challenge/employee/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paramenta.challenge.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
